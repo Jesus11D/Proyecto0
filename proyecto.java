a53cc149eac0a0c7c9bdc0b83241b29f99bbff44
Jesus <jesusdavidcordova11@gmail.com>
2025-08-24 13:37:20 -0500

    Se agrega el intercambio

--- proyecto.java
+++ proyecto.java
@@ -1,9 +1,21 @@
 import javax.swing.*;
 
-public class BurbujaPasoAPaso {
+public class Burbuja {
     public static void main(String[] args) {
         int[] numeros = {5, 3, 8, 4, 2};
 
         JOptionPane.showMessageDialog(null, "Arreglo original: " + arrayToString(numeros));
 
-        burbujaPasoAPaso(numeros);
+        burbuja(numeros);
+
+        JOptionPane.showMessageDialog(null, "Arreglo final ordenado: " + arrayToString(numeros));
+    }
+
+    public static void burbuja(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
